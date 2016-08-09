34923519e41b4d005a1945a92b83281125db5fc4
Robin Andeer <robin.andeer@gmail.com>
2016-08-09 10:26:45 +0200

    add option to only setup db

--- housekeeper/initiate.py
+++ housekeeper/initiate.py
@@ -10,20 +10,11 @@ from housekeeper.store import get_manager, Metadata
 log = logging.getLogger(__name__)
 
 
-def setup(root_path, uri=None):
+def setup(root_path, db_uri):
     """Setup a new structure and database."""
     log.info("create the root directory: %s", root_path)
     abs_root = path(root_path).abspath()
     abs_root.joinpath('analyses').makedirs_p()
-
-    db_uri = uri or "sqlite:///{}".format(abs_root.joinpath('store.sqlite3'))
-    log.info("setup a new database: %s", db_uri)
-    db = get_manager(db_uri)
-    db.create_all()
-
-    log.debug('add metadata about the system')
-    meta = Metadata(root=abs_root)
-    db.add_commit(meta)
 
     config = abs_root.joinpath('housekeeper.yaml')
     log.info("generate a config file: %s", config)
@@ -33,13 +24,29 @@ def setup(root_path, uri=None):
         stream.write(dump.decode('utf-8'))
 
 
+def setup_db(root_path, uri=None):
+    abs_root = path(root_path).abspath()
+    db_uri = uri or "sqlite:///{}".format(abs_root.joinpath('store.sqlite3'))
+    log.info("setup a new database: %s", db_uri)
+    db = get_manager(db_uri)
+    db.create_all()
+
+    log.debug('add metadata about the system')
+    meta = Metadata(root=abs_root)
+    db.add_commit(meta)
+    return db_uri
+
+
 @click.command()
+@click.option('--db-only', is_flag=True)
 @click.argument('root', type=click.Path())
 @click.pass_context
-def init(context, root):
+def init(context, db_only, root):
     """Setup the housekeeper."""
-    if path(root).exists():
-        log.error("root folder already exists: %s", root)
-        context.abort()
-    else:
-        setup(root, uri=context.obj.get('database'))
+    db_uri = setup_db(root, uri=context.obj.get('database'))
+    if not db_only:
+        if path(root).exists():
+            log.error("root folder already exists: %s", root)
+            context.abort()
+        else:
+            setup(root, db_uri)
