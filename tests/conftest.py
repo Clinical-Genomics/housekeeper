aa813144e39e6b2a11577591d2f97274ab63fdf6
Robin Andeer <robin.andeer@gmail.com>
2016-09-22 14:51:19 +0200

    update tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,11 @@ from alchy import Manager
 from click.testing import CliRunner
 from path import path
 import pytest
+import yaml
 
 from housekeeper.cli import root
 from housekeeper.store import api, Model, Metadata
+from housekeeper.pipelines.mip.parse import prepare_inputs
 
 
 @pytest.fixture
@@ -36,6 +38,9 @@ def mip_output():
     mod_qcmetrics = mip_out.joinpath("cust003/16074/analysis/genomes/16074/"
                                      "16074_qcmetrics.mod.yaml")
     mod_qcmetrics.remove_p()
+    meta_suffix = "cust003/16074/analysis/genomes/meta.yaml"
+    new_meta = mip_out.joinpath(meta_suffix)
+    new_meta.remove_p()
 
 
 @pytest.yield_fixture(scope='function')
@@ -60,3 +65,23 @@ def setup_tmp(tmpdir):
     data = dict(uri=uri, path=str(db_path), manager=manager)
     yield data
     manager.drop_all()
+
+
+@pytest.fixture()
+def config_path(mip_output):
+    suffix = "cust003/16074/genomes/16074/16074_config.yaml"
+    _config_path = mip_output.joinpath(suffix)
+    return _config_path
+
+
+@pytest.fixture()
+def config_data(config_path):
+    with open(config_path, "r") as in_handle:
+        _config_data = yaml.load(in_handle)
+    return _config_data
+
+
+@pytest.fixture()
+def segments(config_data):
+    _segments = prepare_inputs(config_data)
+    return _segments
