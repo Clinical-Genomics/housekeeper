38b0c6a89583531b85288c4bd1297bbbccda4cdb
Robin Andeer <robin.andeer@gmail.com>
2016-10-07 13:22:30 +0200

    force skips assets not found on the file system

--- housekeeper/pipelines/mip/core.py
+++ housekeeper/pipelines/mip/core.py
@@ -11,16 +11,16 @@ from .prepare import prepare_run
 log = logging.getLogger(__name__)
 
 
-def build_assets(new_references):
+def build_assets(new_references, force=False):
     """Build assets from references."""
     for ref_data in new_references:
         if not path(ref_data['path']).exists():
             is_required = ref_data['reference'].get('required', True)
-            if is_required:
-                raise MissingFileError(ref_data['path'])
-            else:
+            if force or not is_required:
                 log.warn("skipping asset: %s", ref_data['path'])
                 continue
+            else:
+                raise MissingFileError(ref_data['path'])
 
         new_asset = build_asset(ref_data['path'], ref_data['reference'])
         yield new_asset, ref_data.get('sample')
@@ -43,7 +43,7 @@ def parse_mip(config_data, reference_data, force=False):
     # 4. parse references
     new_refs = parse_references(reference_data, segments)
     # 5. build assets from references + link to new records
-    new_assets = build_assets(new_refs)
+    new_assets = build_assets(new_refs, force=force)
     for new_asset, sample in new_assets:
         link_asset(new_objs['run'], new_asset, sample=sample)
     return new_objs
