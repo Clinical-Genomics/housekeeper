cb2b804a3667ff9c673a67e42c0785f68b4f8445
Kenny Billiau <Kenny.billiau@scilifelab.se>
2017-04-13 13:31:15 +0200

    Fix a simpler encrypt

--- housekeeper/constants.py
+++ housekeeper/constants.py
@@ -4,6 +4,6 @@ from datetime import timedelta
 PIPELINES = ('mip',)
 # three months before clean up by default
 TIME_TO_CLEANUP = timedelta(days=(30 * 3))
-ARCHIVE_TYPES = ('data', 'result', 'meta')
+ARCHIVE_TYPES = ('data', 'result', 'meta', 'archive')
 EXTRA_STATUSES = ['coverage', 'frequency', 'genotype', 'visualizer',
                   'rawdata', 'qc']
