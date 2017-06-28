5afa5b37dad24fc39052b5530f6cbb5d34af7fd2
Robin Andeer <robin.andeer@gmail.com>
2017-06-28 12:54:46 +0200

    use iso string for test of schema

--- tests/add/conftest.py
+++ tests/add/conftest.py
@@ -26,6 +26,6 @@ def bundle_data():
 @pytest.fixture(scope='function')
 def bundle_data_json(bundle_data):
     json_data = deepcopy(bundle_data)
-    json_data['created'] = str(json_data['created'])
-    json_data['expires'] = str(json_data['expires'])
+    json_data['created'] = json_data['created'].isoformat()
+    json_data['expires'] = json_data['expires'].isoformat()
     return json_data
