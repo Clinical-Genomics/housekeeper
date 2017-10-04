6fe0179ed714432488f4ef472ed42ebdd1f18b54
Robin Andeer <robin.andeer@gmail.com>
2017-10-04 10:28:43 +0200

    fix get command, verbose mode

--- housekeeper/cli/get.py
+++ housekeeper/cli/get.py
@@ -8,7 +8,7 @@ from housekeeper.store import Store
 @click.command()
 @click.option('-t', '--tag', 'tags', multiple=True, help='filter by file tag')
 @click.option('-v', '--version', type=int, help='filter by version of the bundle')
-@click.option('--verbose', is_flag=True, help='print additional information')
+@click.option('-V', '--verbose', is_flag=True, help='print additional information')
 @click.argument('bundle', required=False)
 @click.pass_context
 def get(context, tags: List[str], version: int, verbose: bool, bundle: str):
@@ -18,6 +18,7 @@ def get(context, tags: List[str], version: int, verbose: bool, bundle: str):
     for file_obj in files:
         if verbose:
             tags = ', '.join(tag.name for tag in file_obj.tags)
-            click.echo(f"{click.style(file_obj.id, fg='blue')} | {file_obj.full_path} | {tags}")
+            click.echo(f"{click.style(str(file_obj.id), fg='blue')} | {file_obj.full_path} | "
+                       f"{click.style(tags, fg='yellow')}")
         else:
             click.echo(file_obj.full_path)
