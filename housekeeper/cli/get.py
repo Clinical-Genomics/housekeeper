94918e4262ae264b630e3e003a1af35b4efdc3cb
Robin Andeer <robin.andeer@gmail.com>
2017-10-04 10:24:52 +0200

    add verbose option to get

--- housekeeper/cli/get.py
+++ housekeeper/cli/get.py
@@ -8,11 +8,16 @@ from housekeeper.store import Store
 @click.command()
 @click.option('-t', '--tag', 'tags', multiple=True, help='filter by file tag')
 @click.option('-v', '--version', type=int, help='filter by version of the bundle')
+@click.option('--verbose', is_flag=True, help='print additional information')
 @click.argument('bundle', required=False)
 @click.pass_context
-def get(context, bundle: str, tags: List[str], version: int):
+def get(context, tags: List[str], version: int, verbose: bool, bundle: str):
     """Get files."""
     store = Store(context.obj['database'], context.obj['root'])
     files = store.files(bundle=bundle, tags=tags, version=version)
     for file_obj in files:
-        click.echo(file_obj.full_path)
+        if verbose:
+            tags = ', '.join(tag.name for tag in file_obj.tags)
+            click.echo(f"{click.style(file_obj.id, fg='blue')} | {file_obj.full_path} | {tags}")
+        else:
+            click.echo(file_obj.full_path)
