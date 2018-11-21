fff4f137a426d6707d27e306a94ff1e5d0adc75e
Kenny Billiau <kenny.billiau@scilifelab.se>
2018-11-21 21:40:13 +0100

    Add GIVEN-WHEN-THEN statements in test store delete code

--- tests/store/test_store_api.py
+++ tests/store/test_store_api.py
@@ -12,12 +12,17 @@ def test_delete_both(store, bundle_data, bundle_data_old):
     """
     test deletion where all files are older than before date
     """
+
+    # GIVEN two bundles with different creation dates and each two files
     bundle_obj, version_obj = store.add_bundle(data=bundle_data)
     store.add_commit(bundle_obj)
     bundle_old_obj, version_old_obj = store.add_bundle(data=bundle_data_old)
     store.add_commit(bundle_old_obj)
 
+    # WHEN we query for files before now
     query = store.files_before(before=str(datetime.datetime.now()))
+
+    # THEN we should be able to count all files
     assert len(query.all()) == 4
 
 
@@ -25,12 +30,17 @@ def test_delete_one(store, bundle_data, bundle_data_old):
     """
     test deletion where not all files are older than before date
     """
+
+    # GIVEN two bundles with different creation dates and each two files
     bundle_obj, version_obj = store.add_bundle(data=bundle_data)
     store.add_commit(bundle_obj)
     bundle_old_obj, version_old_obj = store.add_bundle(data=bundle_data_old)
     store.add_commit(bundle_old_obj)
 
+    # WHEN we query for all files before just one of the bundles' creation date
     query = store.files_before(before="2018-02-01")
+
+    # THEN we should be able to count two out of four files
     assert len(query.all()) == 2
 
 
@@ -38,12 +48,16 @@ def test_delete_none(store, bundle_data, bundle_data_old):
     """
     test deletion where no files are older than before date
     """
+    # GIVEN two bundles with two different creation dates
     bundle_obj, version_obj = store.add_bundle(data=bundle_data)
     store.add_commit(bundle_obj)
     bundle_old_obj, version_old_obj = store.add_bundle(data=bundle_data_old)
     store.add_commit(bundle_old_obj)
 
+    # WHEN we query for files stored before their creation date
     query = store.files_before(before="2017-02-01")
+
+    # THEN we should not find any files!
     assert len(query.all()) == 0
 
 
@@ -51,13 +65,15 @@ def test_delete_notondisk(store, tmpdir, bundle_data):
     """
     test deletion of files that are not on disk
     """
+    # GIVEN two bundles: one with files on disk and one without
     with tempfile.NamedTemporaryFile(delete=True) as file1:
 
+        # WHEN the files in the bundle are not on disk
         bundle_data_notondisk = deepcopy(bundle_data)
-        bundle_data_notondisk['files'][0]['path']=file1.name
-        bundle_data_notondisk['files'][1]['path']=bundle_data_notondisk['files'][1]['path'].replace('.vcf',
+        bundle_data_notondisk['files'][0]['path'] = file1.name
+        bundle_data_notondisk['files'][1]['path'] = bundle_data_notondisk['files'][1]['path'].replace('.vcf',
                                                                                     '.2.vcf')
-        bundle_data_notondisk['name']='angrybird'
+        bundle_data_notondisk['name'] = 'angrybird'
 
         bundle_obj, version_obj = store.add_bundle(data=bundle_data)
         include_version(tmpdir, version_obj, hardlink=False)
@@ -67,9 +83,9 @@ def test_delete_notondisk(store, tmpdir, bundle_data):
         store.add_commit(bundle_obj_notondisk)
 
     query = store.files_before()
-
-    assert len(query.all()) == 4
-
     files_notondisk = set(query) - store.files_ondisk(query)
 
+    # THEN the amount of files listed in the database ...
+    assert len(query.all()) == 4
+    # ... should be different than the files on disk
     assert len(files_notondisk) == 1
