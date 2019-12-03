47a05238f1c8678e8be8b40dd711cec76343fb90
Barry Stokman <26384108+barrystokman@users.noreply.github.com>
2019-12-03 13:20:09 +0100

    remove dataclass for python 3.6 linter

--- housekeeper/store/api/base.py
+++ housekeeper/store/api/base.py
@@ -1,11 +1,9 @@
 """This module defines a BaseHandler class holding different models"""
 # pylint: disable=invalid-name
-from dataclasses import dataclass
 
 from housekeeper.store import models
 
 
-@dataclass
 class BaseHandler:
     """This is a base class holding different models"""
 
