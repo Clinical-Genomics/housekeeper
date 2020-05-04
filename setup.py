664bf2529bdceecc12fea2c4674c6fe845023bd2
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-05-04 21:53:26 +0200

    Blacks some more files

--- setup.py
+++ setup.py
@@ -4,27 +4,29 @@
 # To use a consistent encoding
 import codecs
 import os
+
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 import sys
 
 # Shortcut for building/publishing to Pypi
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist bdist_wheel upload')
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist bdist_wheel upload")
     sys.exit()
 
 
-def parse_reqs(req_path='./requirements.txt'):
+def parse_reqs(req_path="./requirements.txt"):
     """Recursively parse requirements from nested pip files."""
     install_requires = []
-    with codecs.open(req_path, 'r') as handle:
+    with codecs.open(req_path, "r") as handle:
         # remove comments and empty lines
-        lines = (line.strip() for line in handle
-                 if line.strip() and not line.startswith('#'))
+        lines = (
+            line.strip() for line in handle if line.strip() and not line.startswith("#")
+        )
         for line in lines:
             # check for nested requirements files
-            if line.startswith('-r'):
+            if line.startswith("-r"):
                 # recursively call this function
                 install_requires += parse_reqs(req_path=line[3:])
             else:
@@ -49,89 +51,69 @@ class PyTest(TestCommand):
         """Execute the test runner command."""
         # Import here, because outside the required eggs aren't loaded yet
         import pytest
+
         sys.exit(pytest.main(self.test_args))
 
 
 # Get the long description from the relevant file
 here = os.path.abspath(os.path.dirname(__file__))
-with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 
 setup(
-    name='housekeeper',
-
+    name="housekeeper",
     # Versions should comply with PEP440. For a discussion on
     # single-sourcing the version across setup.py and the project code,
     # see http://packaging.python.org/en/latest/tutorial.html#version
-    version='2.6.1',
-
-    description=('Housekeeper takes care of files.'),
+    version="2.6.1",
+    description=("Housekeeper takes care of files."),
     long_description=long_description,
     # What does your project relate to? Separate with spaces.
-    keywords='housekeeper development',
-    author='Robin Andeer',
-    author_email='robin.andeer@scilifelab.se',
-    license='MIT',
-
+    keywords="housekeeper development",
+    author="Robin Andeer",
+    author_email="robin.andeer@scilifelab.se",
+    license="MIT",
     # The project's main homepage
-    url='https://github.com/Clinical-Genomics/housekeeper',
-
-    packages=find_packages(exclude=('tests*', 'docs', 'examples')),
-
+    url="https://github.com/Clinical-Genomics/housekeeper",
+    packages=find_packages(exclude=("tests*", "docs", "examples")),
     # If there are data files included in your packages that need to be
     # installed, specify them here.
     include_package_data=True,
     zip_safe=False,
-
     # Although 'package_data' is the preferred approach, in some case you
     # may need to place data files outside of your packages.
     # In this case, 'data_file' will be installed into:
     # '<sys.prefix>/my_data'
     # data_files=[('my_data', ['data/data_file'])],
-
     # Install requirements loaded from ``requirements.txt``
     install_requires=parse_reqs(),
-    tests_require=[
-        'pytest',
-    ],
-    cmdclass=dict(
-        test=PyTest,
-    ),
-
+    tests_require=["pytest",],
+    cmdclass=dict(test=PyTest,),
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and
     # allow pip to create the appropriate form of executable for the
     # target platform.
-    entry_points={
-        'console_scripts': [
-            'housekeeper = housekeeper.cli:base',
-        ],
-    },
-
+    entry_points={"console_scripts": ["housekeeper = housekeeper.cli:base",],},
     # See: http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         # How mature is this project? Common values are:
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
+        "Development Status :: 3 - Alpha",
         # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development',
-
+        "Intended Audience :: Developers",
+        "Topic :: Software Development",
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
-
+        "License :: OSI Approved :: MIT License",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-
-        'Environment :: Console',
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Environment :: Console",
     ],
 )
--- tests/store/test_add_schema.py
+++ tests/store/test_add_schema.py
@@ -12,9 +12,9 @@ def test_schema_with_valid_input(bundle_data_json):
 
 def test_schema_with_invalid_input(bundle_data_json):
     # GIVEN input data with missing name of the bundle
-    del bundle_data_json['name']
+    del bundle_data_json["name"]
     # WHEN validating it against the schema
     errors = schema.BundleSchema().validate(bundle_data_json)
     # THEN it should report errors for the field
     assert len(errors) > 0
-    assert 'name' in errors
+    assert "name" in errors
