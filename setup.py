68f621af15568e44a758837e58b8929e55158e45
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-09-07 15:50:12 +0200

    build and publish to pypi (#77)
    * Adds workflow to publish to pypi
    
    * Adds workflow for changelog reminder

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 """Based on https://github.com/pypa/sampleproject/blob/master/setup.py."""
+import io
+
 # To use a consistent encoding
-import codecs
 import os
 
 # Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
-import sys
+from setuptools import find_packages, setup
 
-# Shortcut for building/publishing to Pypi
-if sys.argv[-1] == "publish":
-    os.system("python setup.py sdist bdist_wheel upload")
-    sys.exit()
+NAME = "housekeeper"
+DESCRIPTION = "Housekeeper takes care of files"
+AUTHOR = "Robin Andeer"
+EMAIL = "mans.magnusson@scilifelab.se"
+URL = "https://github.com/Clinical-Genomics/housekeeper"
+
+HERE = os.path.abspath(os.path.dirname(__file__))
 
 
 def parse_reqs(req_path="./requirements.txt"):
     """Recursively parse requirements from nested pip files."""
     install_requires = []
-    with codecs.open(req_path, "r") as handle:
+    with open(req_path, "r") as handle:
         # remove comments and empty lines
         lines = (
             line.strip() for line in handle if line.strip() and not line.startswith("#")
@@ -35,47 +35,30 @@ def parse_reqs(req_path="./requirements.txt"):
     return install_requires
 
 
-# This is a plug-in for setuptools that will invoke py.test
-# when you run python setup.py test
-class PyTest(TestCommand):
-
-    """Set up the py.test test runner."""
-
-    def finalize_options(self):
-        """Set options for the command line."""
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        """Execute the test runner command."""
-        # Import here, because outside the required eggs aren't loaded yet
-        import pytest
-
-        sys.exit(pytest.main(self.test_args))
-
-
-# Get the long description from the relevant file
-here = os.path.abspath(os.path.dirname(__file__))
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(HERE, "README.md"), encoding="utf-8") as f:
+        LONG_DESCRIPTION = "\n" + f.read()
+except FileNotFoundError:
+    LONG_DESCRIPTION = DESCRIPTION
 
 
 setup(
-    name="housekeeper",
+    name=NAME,
     # Versions should comply with PEP440. For a discussion on
     # single-sourcing the version across setup.py and the project code,
     # see http://packaging.python.org/en/latest/tutorial.html#version
     version="2.7.4",
-    description=("Housekeeper takes care of files."),
-    long_description=long_description,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
     # What does your project relate to? Separate with spaces.
     keywords="housekeeper development",
-    author="Robin Andeer",
-    author_email="robin.andeer@scilifelab.se",
+    author=AUTHOR,
+    author_email=EMAIL,
     license="MIT",
     # The project's main homepage
-    url="https://github.com/Clinical-Genomics/housekeeper",
+    url=URL,
     packages=find_packages(exclude=("tests*", "docs", "examples")),
     # If there are data files included in your packages that need to be
     # installed, specify them here.
@@ -88,13 +71,11 @@ setup(
     # data_files=[('my_data', ['data/data_file'])],
     # Install requirements loaded from ``requirements.txt``
     install_requires=parse_reqs(),
-    tests_require=["pytest",],
-    cmdclass=dict(test=PyTest,),
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and
     # allow pip to create the appropriate form of executable for the
     # target platform.
-    entry_points={"console_scripts": ["housekeeper = housekeeper.cli:base",],},
+    entry_points={"console_scripts": ["housekeeper = housekeeper.cli:base"]},
     # See: http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         # How mature is this project? Common values are:
@@ -107,13 +88,8 @@ setup(
         "Topic :: Software Development",
         # Pick your license as you wish (should match "license" above)
         "License :: OSI Approved :: MIT License",
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.6",
         "Environment :: Console",
     ],
 )
