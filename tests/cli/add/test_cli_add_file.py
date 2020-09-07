7ae4e7b9ed3bf642335f0d42c34303acad0485b1
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-09-07 15:50:08 +0200

    Adds tests for add file cli

--- tests/cli/add/test_cli_add_file.py
+++ tests/cli/add/test_cli_add_file.py
@@ -1,11 +1,19 @@
 """Tests foe adding files via CLI"""
 import logging
+from pathlib import Path
+
+from click import Context
+from click.testing import CliRunner
 
 from housekeeper.cli.add import file_cmd
 
 
 def test_add_file_non_existing_bundle(
-    base_context, cli_runner, case_id, second_sample_vcf, caplog
+    base_context: Context,
+    cli_runner: CliRunner,
+    case_id: str,
+    second_sample_vcf: Path,
+    caplog,
 ):
     """Test to add a file to a non existing bundle"""
     caplog.set_level(logging.DEBUG)
@@ -23,8 +31,26 @@ def test_add_file_non_existing_bundle(
     assert f"unknown bundle: {unknown_bundle_name}" in caplog.text
 
 
+def test_add_file_no_input(base_context: Context, cli_runner: CliRunner, caplog):
+    """Test to add a file without specifying any input"""
+    caplog.set_level(logging.DEBUG)
+    # GIVEN a context with a empty store and a cli runner
+
+    # WHEN trying to add a bundle
+    result = cli_runner.invoke(file_cmd, [], obj=base_context)
+
+    # THEN assert it fails since there is no valid input
+    assert result.exit_code == 1
+    # THEN check that the proper information is displayed
+    assert "Please input json or path" in caplog.text
+
+
 def test_add_file_existing_bundle(
-    populated_context, cli_runner, case_id, second_sample_vcf, caplog
+    populated_context: CliRunner,
+    cli_runner: CliRunner,
+    case_id: str,
+    second_sample_vcf: Path,
+    caplog,
 ):
     """Test to add a file to a existing bundle"""
     caplog.set_level(logging.DEBUG)
