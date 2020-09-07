c6610befad95ebfd64d8a7ad6166b5deb1051f62
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-09-07 14:34:29 +0200

    Blacks some files

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,10 @@
 import copy
 import datetime
 import json
-import logging
 import shutil
 from copy import deepcopy
 from pathlib import Path
+from typing import List
 
 import pytest
 import ruamel.yaml
@@ -15,14 +15,6 @@ from housekeeper.date import get_date
 from housekeeper.store import Store, models
 
 from .helper_functions import Helpers
-
-
-@pytest.fixture(scope="function", name="log_output")
-def fixture_log_output(caplog):
-    """Return the a formated caplog object"""
-    caplog.set_level(logging.DEBUG)
-    return caplog
-
 
 # basic fixtures
 
@@ -46,13 +38,13 @@ def fixture_sample_tag_name() -> str:
 
 
 @pytest.fixture(scope="function", name="family_tag_names")
-def fixture_family_tag_names(vcf_tag_name, family_tag_name) -> list:
+def fixture_family_tag_names(vcf_tag_name: str, family_tag_name: str) -> List[str]:
     """Return a list of the family tag names"""
     return [vcf_tag_name, family_tag_name]
 
 
 @pytest.fixture(scope="function", name="sample_tag_names")
-def fixture_sample_tag_names(vcf_tag_name, sample_tag_name) -> list:
+def fixture_sample_tag_names(vcf_tag_name: str, sample_tag_name: str) -> List[str]:
     """Return a list of the sample tag names"""
     return [vcf_tag_name, sample_tag_name]
 
@@ -70,25 +62,25 @@ def fixture_other_case_id() -> str:
 
 
 @pytest.fixture(scope="function", name="sample_data")
-def fixture_sample_data(sample_tag_names, sample_vcf) -> str:
+def fixture_sample_data(sample_tag_names: List[str], sample_vcf: Path) -> dict:
     """Return file and tags for sample"""
     return {"tags": sample_tag_names, "file": sample_vcf}
 
 
 @pytest.fixture(scope="function", name="sample2_data")
-def fixture_sample2_data(sample_tag_names, second_sample_vcf) -> str:
+def fixture_sample2_data(sample_tag_names: List[str], second_sample_vcf: Path) -> dict:
     """Return file and tags for sample"""
     return {"tags": sample_tag_names, "file": second_sample_vcf}
 
 
 @pytest.fixture(scope="function", name="family_data")
-def fixture_family_data(family_tag_names, family_vcf) -> str:
+def fixture_family_data(family_tag_names: List[str], family_vcf: Path) -> dict:
     """Return file and tags for sample"""
     return {"tags": family_tag_names, "file": family_vcf}
 
 
 @pytest.fixture(scope="function", name="family2_data")
-def fixture_family2_data(family_tag_names, second_family_vcf) -> str:
+def fixture_family2_data(family_tag_names: List[str], second_family_vcf: Path) -> dict:
     """Return file and tags for sample"""
     return {"tags": family_tag_names, "file": second_family_vcf}
 
@@ -100,7 +92,7 @@ def fixture_timestamp_string() -> str:
 
 
 @pytest.fixture(scope="function", name="timestamp")
-def fixture_timestamp(timestamp_string) -> datetime.datetime:
+def fixture_timestamp(timestamp_string: str) -> datetime.datetime:
     """Return a time stamp in date time format"""
     return get_date(timestamp_string)
 
@@ -112,7 +104,9 @@ def fixture_later_timestamp() -> datetime.datetime:
 
 
 @pytest.fixture(scope="function", name="bundle_data")
-def fixture_bundle_data(case_id, sample_data, family_data, timestamp) -> dict:
+def fixture_bundle_data(
+    case_id: str, sample_data: dict, family_data: dict, timestamp: datetime.datetime
+) -> dict:
     """Return a dummy bundle"""
     data = {
         "name": case_id,
@@ -134,14 +128,14 @@ def fixture_bundle_data(case_id, sample_data, family_data, timestamp) -> dict:
 
 
 @pytest.fixture(scope="function", name="empty_version_data")
-def fixture_empty_version_data(later_timestamp, case_id) -> dict:
+def fixture_empty_version_data(later_timestamp: datetime.datetime, case_id: str) -> dict:
     """Return a dummy bundle"""
     data = {"bundle_name": case_id, "created_at": later_timestamp, "files": []}
     return data
 
 
 @pytest.fixture(scope="function", name="version_data")
-def fixture_version_data(empty_version_data, family2_data, sample2_data) -> dict:
+def fixture_version_data(empty_version_data: dict, family2_data: dict, sample2_data: dict) -> dict:
     """Return a dummy bundle"""
     data = copy.deepcopy(empty_version_data)
     data["files"] = [
@@ -160,7 +154,7 @@ def fixture_version_data(empty_version_data, family2_data, sample2_data) -> dict
 
 
 @pytest.fixture(scope="function", name="bundle_data_json")
-def fixture_bundle_data_json(bundle_data) -> dict:
+def fixture_bundle_data_json(bundle_data: dict) -> str:
     """Return a dummy bundle"""
     json_data = copy.deepcopy(bundle_data)
     json_data["created_at"] = str(json_data.pop("created_at"))
@@ -168,7 +162,7 @@ def fixture_bundle_data_json(bundle_data) -> dict:
 
 
 @pytest.fixture(scope="function", name="empty_version_data_json")
-def fixture_empty_version_data_json(empty_version_data) -> dict:
+def fixture_empty_version_data_json(empty_version_data: dict) -> str:
     """Return a dummy bundle"""
     json_data = copy.deepcopy(empty_version_data)
     json_data["created_at"] = str(json_data.pop("created_at"))
@@ -176,7 +170,7 @@ def fixture_empty_version_data_json(empty_version_data) -> dict:
 
 
 @pytest.fixture(scope="function", name="version_data_json")
-def fixture_version_data_json(version_data) -> dict:
+def fixture_version_data_json(version_data: dict) -> str:
     """Return a dummy bundle"""
     json_data = copy.deepcopy(version_data)
     json_data["created_at"] = str(json_data.pop("created_at"))
@@ -185,7 +179,11 @@ def fixture_version_data_json(version_data) -> dict:
 
 @pytest.fixture(scope="function", name="other_bundle")
 def fixture_other_bundle(
-    bundle_data, other_case_id, later_timestamp, second_sample_vcf, second_family_vcf
+    bundle_data: dict,
+    other_case_id: str,
+    later_timestamp: datetime.datetime,
+    second_sample_vcf: Path,
+    second_family_vcf: Path,
 ) -> dict:
     """Return a dummy bundle"""
     data = deepcopy(bundle_data)
@@ -203,25 +201,25 @@ def fixture_db_name() -> str:
 
 
 @pytest.fixture(scope="function", name="db_path")
-def fixture_db_path(db_dir, db_name) -> Path:
+def fixture_db_path(db_dir: Path, db_name: str) -> Path:
     """Return the path to a database"""
     return db_dir / db_name
 
 
 @pytest.fixture(scope="function", name="db_uri")
-def fixture_db_uri(db_path) -> Path:
+def fixture_db_uri(db_path: Path) -> str:
     """Return the uri to an in memory database"""
     return "sqlite:///" + str(db_path)
 
 
 @pytest.fixture(scope="function", name="db_uri_memory")
-def fixture_db_uri_memory() -> Path:
+def fixture_db_uri_memory() -> str:
     """Return the uri to an in memory database"""
     return "sqlite:///:memory:"
 
 
 @pytest.fixture(scope="function", name="configs")
-def fixture_configs(project_dir, db_uri) -> dict:
+def fixture_configs(project_dir: Path, db_uri: str) -> dict:
     """Return a dict with housekeeper configs"""
     _configs = {"root": str(project_dir), "database": db_uri}
     return _configs
@@ -231,13 +229,13 @@ def fixture_configs(project_dir, db_uri) -> dict:
 
 
 @pytest.fixture(scope="function", name="vcf_tag_obj")
-def fixture_vcf_tag_obj(vcf_tag_name, timestamp) -> str:
+def fixture_vcf_tag_obj(vcf_tag_name: str, timestamp: datetime.datetime) -> str:
     """Return a tag object"""
     return models.Tag(name=vcf_tag_name, created_at=timestamp)
 
 
 @pytest.fixture(scope="function", name="bundle_obj")
-def fixture_bundle_obj(bundle_data, store) -> str:
+def fixture_bundle_obj(bundle_data: dict, store: Store) -> str:
     """Return a bundle object"""
     return store.add_bundle(bundle_data)[0]
 
@@ -252,13 +250,13 @@ def fixture_fixtures_dir() -> Path:
 
 
 @pytest.fixture(scope="function", name="vcf_dir")
-def fixture_vcf_dir(fixtures_dir) -> Path:
+def fixture_vcf_dir(fixtures_dir: Path) -> Path:
     """Return the path to the vcf fixtures directory"""
     return fixtures_dir / "vcfs"
 
 
 @pytest.fixture(scope="function", name="project_dir")
-def fixture_project_dir(tmpdir_factory):
+def fixture_project_dir(tmpdir_factory) -> Path:
     """Path to a temporary working directory"""
     my_tmpdir = Path(tmpdir_factory.mktemp("workdir"))
     yield my_tmpdir
@@ -266,7 +264,7 @@ def fixture_project_dir(tmpdir_factory):
 
 
 @pytest.fixture(scope="function", name="config_dir")
-def fixture_config_dir(tmpdir_factory):
+def fixture_config_dir(tmpdir_factory) -> Path:
     """Path to a temporary directory for config files"""
     my_tmpdir = Path(tmpdir_factory.mktemp("confdir"))
     yield my_tmpdir
@@ -274,7 +272,7 @@ def fixture_config_dir(tmpdir_factory):
 
 
 @pytest.fixture(scope="function", name="db_dir")
-def fixture_db_dir(tmpdir_factory):
+def fixture_db_dir(tmpdir_factory) -> Path:
     """Path to a temporary directory for databases"""
     my_tmpdir = Path(tmpdir_factory.mktemp("db_dir"))
     yield my_tmpdir
@@ -285,7 +283,7 @@ def fixture_db_dir(tmpdir_factory):
 
 
 @pytest.fixture(scope="function", name="config_file")
-def fixture_config_file(config_dir, configs) -> Path:
+def fixture_config_file(config_dir: Path, configs: dict) -> Path:
     """Create a config file and return the path to it"""
     conf_path = config_dir / "config.json"
     with open(conf_path, "w") as out_file:
@@ -294,43 +292,43 @@ def fixture_config_file(config_dir, configs) -> Path:
 
 
 @pytest.fixture(scope="function", name="sample_vcf")
-def fixture_sample_vcf(vcf_dir) -> Path:
+def fixture_sample_vcf(vcf_dir: Path) -> Path:
     """Return the path to a vcf file"""
     return vcf_dir / "example.vcf"
 
 
 @pytest.fixture(scope="function", name="family_vcf")
-def fixture_family_vcf(vcf_dir) -> Path:
+def fixture_family_vcf(vcf_dir: Path) -> Path:
     """Return the path to a vcf file"""
     return vcf_dir / "family.vcf"
 
 
 @pytest.fixture(scope="function", name="second_sample_vcf")
-def fixture_second_sample_vcf(vcf_dir) -> Path:
+def fixture_second_sample_vcf(vcf_dir: Path) -> Path:
     """Return the path to a vcf file"""
     return vcf_dir / "example.2.vcf"
 
 
 @pytest.fixture(scope="function", name="second_family_vcf")
-def fixture_second_family_vcf(vcf_dir) -> Path:
+def fixture_second_family_vcf(vcf_dir: Path) -> Path:
     """Return the path to a vcf file"""
     return vcf_dir / "family.2.vcf"
 
 
 @pytest.fixture(scope="function", name="checksum_file")
-def fixture_checksum_file(fixtures_dir) -> Path:
+def fixture_checksum_file(fixtures_dir: Path) -> Path:
     """Return the path to file to test checksum"""
     return fixtures_dir / "26a90105b99c05381328317f913e9509e373b64f.txt"
 
 
 @pytest.fixture(scope="function", name="checksum")
-def fixture_checksum(checksum_file) -> Path:
+def fixture_checksum(checksum_file: Path) -> Path:
     """Return the checksum for checksum test file"""
     return checksum_file.name.rstrip(".txt")
 
 
 @pytest.fixture(scope="function", name="version")
-def fixture_version(tmpdir):
+def fixture_version(tmpdir: Path) -> models.Version:
     """Return a version object"""
     file_path_1 = tmpdir.join("example.vcf.gz")
     file_path_1.write("content")
@@ -354,7 +352,7 @@ def fixture_version(tmpdir):
 
 
 @pytest.fixture(scope="function", name="helpers")
-def fixture_helpers() -> Path:
+def fixture_helpers() -> Helpers:
     """Return a test helper object"""
     return Helpers()
 
@@ -363,7 +361,7 @@ def fixture_helpers() -> Path:
 
 
 @pytest.yield_fixture(scope="function", name="store")
-def fixture_store(project_dir):
+def fixture_store(project_dir: Path) -> Store:
     """Return a store setup with all tables"""
     _store = Store(uri="sqlite:///", root=str(project_dir))
     _store.create_all()
@@ -372,7 +370,7 @@ def fixture_store(project_dir):
 
 
 @pytest.yield_fixture(scope="function", name="populated_store")
-def fixture_populated_store(store, bundle_data, helpers):
+def fixture_populated_store(store: Store, bundle_data: dict, helpers: Helpers) -> Store:
     """Returns a populated store"""
     helpers.add_bundle(store, bundle_data)
     return store
