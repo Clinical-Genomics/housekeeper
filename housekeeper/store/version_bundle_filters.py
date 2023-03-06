d8dea116d69eac32b868d4fe2410e9ff75243d80
seallard <seallards@gmail.com>
2023-03-06 17:08:30 +0100

    Add VersionBundle filter functions

---
+++ housekeeper/store/version_bundle_filters.py
@@ -0,0 +1,31 @@
+from enum import Enum
+from typing import Callable, List, Optional
+from alchy import Query
+import datetime as dt
+
+from housekeeper.store.models import Bundle, Version
+
+
+def filter_version_by_name_and_bundle_date(version_bundles: Query, bundle_name: str, version_date: dt.datetime, **kwargs) -> Query:
+    """Return version by date and bundle name."""
+    return version_bundles.filter(Bundle.name == bundle_name, Version.created_at == version_date)
+
+
+class VersionBundleFilters(Enum):
+    """Define Version filter functions."""
+    FILTER_BY_NAME_AND_DATE: Callable = filter_version_by_name_and_bundle_date
+
+def apply_version_bundle_filter(
+    version_bundles: Query,
+    filter_functions: List[Callable],
+    bundle_name: Optional[str] = None,
+    version_date: Optional[dt.datetime] = None,
+) -> Query:
+    """Apply filtering functions and return filtered results."""
+    for filter_function in filter_functions:
+        version_bundles: Query = filter_function(
+            version_bundles=version_bundles,
+            bundle_name=bundle_name,
+            version_date=version_date,
+        )
+    return version_bundles
