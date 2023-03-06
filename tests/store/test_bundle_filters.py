255d562622e8ad871dbab796d262750ba22117b0
seallard <seallards@gmail.com>
2023-03-06 15:15:40 +0100

    Assert store is non empty in tests

--- tests/store/test_bundle_filters.py
+++ tests/store/test_bundle_filters.py
@@ -6,32 +6,42 @@ from housekeeper.store.bundle_filters import filter_bundle_by_name, filter_bundl
 
 def test_filter_bundles_by_id(populated_store: Store):
     """Test getting collaboration by internal_id."""
+
     # GIVEN a store with a bundle
-    bundle_id: int = 1
+    bundle: Bundle = populated_store.session.query(Bundle).first()
+    assert bundle
+
+    bundle_id: int = bundle.id
+
     # WHEN retrieving the bundle by id
     bundle: Bundle = filter_bundle_by_id(
         bundles=populated_store._get_bundle_query(),
         bundle_id=bundle_id,
     ).first()
 
-    # THEN bundle should be returned
+    # THEN a bundle should be returned
     assert bundle
 
-    # THEN the internal_id should match the fixture
+    # THEN the id should match
     assert bundle.id == bundle_id
 
-def test_filter_bundles_by_name(populated_store: Store, case_id: str):
+def test_filter_bundles_by_name(populated_store: Store):
     """Test getting bundle by name."""
+
     # GIVEN a store with a bundle
+    bundle: Bundle = populated_store.session.query(Bundle).first()
+    assert bundle
+
+    bundle_name: str = bundle.name
 
     # WHEN retrieving the bundle by name
     bundle: Bundle = filter_bundle_by_name(
         bundles=populated_store._get_bundle_query(),
-        bundle_name=case_id,
+        bundle_name=bundle_name,
     ).first()
 
-    # THEN bundle should be returned
+    # THEN a bundle should be returned
     assert bundle
 
-    # THEN the internal_id should match the fixture
-    assert bundle.name == case_id
+    # THEN the name should match
+    assert bundle.name == bundle_name
