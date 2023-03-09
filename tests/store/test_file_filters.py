252067f926aeb3fb6c00856519312472868d80a9
seallard <seallards@gmail.com>
2023-03-09 08:30:20 +0100

    Add test for filtering files on tags

--- tests/store/test_file_filters.py
+++ tests/store/test_file_filters.py
@@ -1,7 +1,8 @@
 
+from typing import List
 from housekeeper.store import Store
 from housekeeper.store.models import File
-from housekeeper.store.file_filters import filter_files_by_id
+from housekeeper.store.file_filters import filter_files_by_id, filter_files_by_tags
 
 
 def test_filter_files_by_id_returns_the_correct_file(populated_store: Store):
@@ -24,3 +25,24 @@ def test_filter_files_by_id_returns_the_correct_file(populated_store: Store):
 
     # THEN the id should match
     assert file.id == file_id
+
+def test_filter_files_by_tags_returns_correct_files(populated_store: Store):
+    """Test filtering files by tags."""
+
+    # GIVEN a store with files
+    file: File = populated_store._get_file_query().first()
+    tag_names: List[str] = [tag.name for tag in file.tags]
+
+    # WHEN filtering files by tags
+    filtered_files_query = filter_files_by_tags(
+        files=populated_store._get_file_query(),
+        tags=tag_names,
+    )
+
+    filtered_files: List[File] = filtered_files_query.all()
+
+    # THEN each file should have all the requested tags
+    for file in filtered_files:
+        assert all(
+            tag.name in tag_names for tag in file.tags
+        )
