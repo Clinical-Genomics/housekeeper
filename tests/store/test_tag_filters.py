284280ee8c18a3c5141dcca3197a0ccaace0486b
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2023-03-13 15:51:07 +0100

    Fix tag filter test function

--- tests/store/test_tag_filters.py
+++ tests/store/test_tag_filters.py
@@ -1,25 +1,25 @@
 from housekeeper.store import Store
 from housekeeper.store.models import Tag
 from housekeeper.store.tag_filters import filter_tag_by_name
+from sqlalchemy.orm import Query
 
 
-def test_filter_tag_by_id_returns_the_correct_tag(populated_store: Store):
-    """Test getting collaboration by internal_id."""
+def test_filter_tag_by_name_returns_correct_tag(populated_store: Store, sample_tag_name: str):
+    """Test that fetching a tag by name returns a tag with the expected name."""
 
-    # GIVEN a store with a tag
-    tag: Tag = populated_store.session.query(Tag).first()
-    assert tag
-
-    tag_name: str = tag.name
+    # GIVEN a store with tags
+    tags: Query = populated_store._get_tag_query()
+    # tag: Tag = populated_store.session.query(Tag).first()
+    assert tags.count() > 0
 
     # WHEN retrieving the tag by name
     tag: Tag = filter_tag_by_name(
-        tags=populated_store._get_tag_query(),
-        tag_name=tag_name,
+        tags=tags,
+        tag_name=sample_tag_name,
     ).first()
 
     # THEN a tag should be returned
     assert isinstance(tag, Tag)
 
     # THEN the name should match
-    assert tag.name == tag_name
+    assert tag.name == sample_tag_name
