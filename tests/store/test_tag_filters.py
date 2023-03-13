6500b8a5a9dda3cc344a9944010a66e5572e83bd
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2023-03-13 16:01:33 +0100

    Fixed completely the tag filter test

--- tests/store/test_tag_filters.py
+++ tests/store/test_tag_filters.py
@@ -2,14 +2,16 @@ from housekeeper.store import Store
 from housekeeper.store.models import Tag
 from housekeeper.store.tag_filters import filter_tag_by_name
 from sqlalchemy.orm import Query
+from typing import List
 
 
 def test_filter_tag_by_name_returns_correct_tag(populated_store: Store, sample_tag_name: str):
     """Test that fetching a tag by name returns a tag with the expected name."""
 
-    # GIVEN a store with tags
+    # GIVEN a store with the desired tag
     tags: Query = populated_store._get_tag_query()
-    assert tags.count() > 0
+    tag_names: List[str] = [t.name for t in tags.all()]
+    assert sample_tag_name in tag_names
 
     # WHEN retrieving the tag by name
     tag: Tag = filter_tag_by_name(
