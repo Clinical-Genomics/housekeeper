aca9ed12f7b262807ea2bede8f32a432006742ed
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2023-03-14 13:13:02 +0100

    Fix add tag tests

--- tests/cli/get/test_get_tag.py
+++ tests/cli/get/test_get_tag.py
@@ -4,19 +4,22 @@ import json
 import logging
 
 from click import Context
-from click.testing import CliRunner
+from click.testing import CliRunner, Result
 
 from housekeeper.cli.add import tag_cmd as add_tags
 from housekeeper.cli.get import tag_cmd
 
+from sqlalchemy.orm import Query
+from typing import List
+
 
 def test_get_tags_empty(cli_runner: Context, base_context: CliRunner, caplog):
-    """Test to get tags from a empty database"""
+    """Test to get tags from an empty database"""
     caplog.set_level(logging.DEBUG)
-    # GIVEN a empty database, log_output and a cli runner
+    # GIVEN an empty database, log_output and a cli runner
 
     # WHEN getting all tags
-    res = cli_runner.invoke(tag_cmd, obj=base_context)
+    res: Result = cli_runner.invoke(tag_cmd, obj=base_context)
 
     # THEN assert it exits without problems
     assert res.exit_code == 0
@@ -28,29 +31,29 @@ def test_get_tags(cli_runner: Context, base_context: CliRunner, caplog):
     """Test to get tags from a database with some tags"""
     caplog.set_level(logging.DEBUG)
     # GIVEN a database with some tags
-    tags = ["tag1", "tag2", "tag3"]
-    cli_runner.invoke(add_tags, tags, obj=base_context)
-    tag_objs = base_context["store"].get_tags()
-    assert tag_objs
+    tag_names: List[str] = ["tag1", "tag2", "tag3"]
+    cli_runner.invoke(add_tags, tag_names, obj=base_context)
+    tag_query: Query = base_context["store"].get_tags()
+    assert tag_query
 
     # WHEN getting all tags
-    res = cli_runner.invoke(tag_cmd, obj=base_context)
+    res: Result = cli_runner.invoke(tag_cmd, obj=base_context)
 
     # THEN assert it exits without problems
     assert res.exit_code == 0
-    assert tags[0] in caplog.text
+    assert tag_names[0] in caplog.text
 
 
-def test_get_tags_non_existing(cli_runner: Context, base_context: CliRunner, caplog):
-    """Test to get a non existing tag from a database with some tags"""
+def test_get_tags_non_existing(cli_runner: Context, base_context: CliRunner, non_existent_tag_name, caplog):
+    """Test to get a non-existing tag from a database with some tags"""
     caplog.set_level(logging.DEBUG)
     # GIVEN a database with some tags
-    tags = ["tag1", "tag2", "tag3"]
-    cli_runner.invoke(add_tags, tags, obj=base_context)
+    tag_names: List[str] = ["tag1", "tag2", "tag3"]
+    cli_runner.invoke(add_tags, tag_names, obj=base_context)
 
     # WHEN getting tag that does not exist
-    other_tag = "nonexisting"
-    res = cli_runner.invoke(tag_cmd, ["-n", other_tag], obj=base_context)
+    other_tag: str = non_existent_tag_name
+    res: Result = cli_runner.invoke(tag_cmd, ["-n", other_tag], obj=base_context)
 
     # THEN assert it exits without problems
     assert res.exit_code == 0
@@ -61,13 +64,13 @@ def test_get_tags_json(cli_runner: Context, base_context: CliRunner, caplog):
     """Test to get tags from a database with some tags in json format"""
     caplog.set_level(logging.DEBUG)
     # GIVEN a database with some tags
-    tags = ["tag1", "tag2", "tag3"]
-    cli_runner.invoke(add_tags, tags, obj=base_context)
-    tag_objs = base_context["store"].get_tags()
-    assert tag_objs
+    tag_names: List[str] = ["tag1", "tag2", "tag3"]
+    cli_runner.invoke(add_tags, tag_names, obj=base_context)
+    tag_query: Query = base_context["store"].get_tags()
+    assert tag_query
 
     # WHEN getting all tags
-    res = cli_runner.invoke(tag_cmd, ["--json"], obj=base_context)
+    res: Result = cli_runner.invoke(tag_cmd, ["--json"], obj=base_context)
 
     # THEN assert it exits without problems
     assert res.exit_code == 0
