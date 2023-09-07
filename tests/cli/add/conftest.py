2c676e571301fe8f7ab9757c917eb53905170c44
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2023-09-07 13:50:58 +0200

    feat(refactor):

--- tests/cli/add/conftest.py
+++ tests/cli/add/conftest.py
@@ -6,13 +6,13 @@ from typing import List
 import pytest
 
 
-@pytest.fixture(name="file_data")
-def fixture_file_data(second_sample_vcf: Path, case_id: str, sample_tag_names: List[str]) -> dict:
+@pytest.fixture()
+def file_data(second_sample_vcf: Path, case_id: str, sample_tag_names: List[str]) -> dict:
     """Return a dictionary with file information"""
     return {"path": str(second_sample_vcf), "tags": sample_tag_names, "bundle": case_id}
 
 
-@pytest.fixture(name="file_data_json")
-def fixture_file_data_json(file_data) -> str:
+@pytest.fixture()
+def file_data_json(file_data) -> str:
     """Return a json string with file information"""
     return json.dumps(file_data)
