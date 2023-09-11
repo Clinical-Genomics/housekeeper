ca1d4e40f2bf63c0d183811e90514469df1504bc
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2023-09-11 08:04:09 +0200

    fix(review):

--- tests/cli/add/conftest.py
+++ tests/cli/add/conftest.py
@@ -6,13 +6,13 @@ from typing import List
 import pytest
 
 
-@pytest.fixture()
+@pytest.fixture
 def file_data(second_sample_vcf: Path, case_id: str, sample_tag_names: List[str]) -> dict:
     """Return a dictionary with file information"""
     return {"path": str(second_sample_vcf), "tags": sample_tag_names, "bundle": case_id}
 
 
-@pytest.fixture()
+@pytest.fixture
 def file_data_json(file_data) -> str:
     """Return a json string with file information"""
     return json.dumps(file_data)
