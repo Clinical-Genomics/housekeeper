6ef60df12a1b82855789e8f480454be5b4ae7568
seallard <sebastian.allard@scilifelab.se>
2024-04-29 11:17:09 +0200

    Fix parameter name

--- housekeeper/services/file_service/file_service.py
+++ housekeeper/services/file_service/file_service.py
@@ -9,10 +9,10 @@ class FileService:
 
     def get_local_files(self, bundle: str, tags: list[str], version_id: int) -> list[File]:
         return self.store.get_files(
-            bundle_name=bundle, tag_names=tags, version_id=version_id, is_local=True
+            bundle_name=bundle, tag_names=tags, version_id=version_id, local_only=True
         )
 
     def get_remote_files(self, bundle: str, tags: list[str], version_id: int) -> list[File]:
         return self.store.get_files(
-            bundle_name=bundle, tag_names=tags, version_id=version_id, is_remote=True
+            bundle_name=bundle, tag_names=tags, version_id=version_id, remote_only=True
         )
